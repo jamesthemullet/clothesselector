19e8c0ccc96b05da38d5b330cb8824368f29f848
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2022-02-03 10:23:31 +0000

    Randomise outfit
    Randomise outfit

--- clothes-selector/src/components/SelectWardrobe/index.tsx
+++ clothes-selector/src/components/SelectWardrobe/index.tsx
@@ -2,32 +2,37 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const SelectWardrobe = () => {
-  const [myBottoms, setMyBottoms] = useState([]);
-  const [myTops, setmyTops] = useState([]);
-  const [myFeet, setmyFeet] = useState([]);
-  const [myOuterwear, setmyOuterwear] = useState([]);
+  const [myOutfit, setMyOutfit] = useState([]);
   const location = useLocation();
-  console.log(50, location);
   const {
     data: { male },
   } = location.state;
-  console.log(51, male);
 
   const chooseMyOutfit = () => {
-    console.log(999, male);
-    setMyBottoms(male.bottom[0].type);
-    setmyTops(male.top[0].type);
-    setmyFeet(male.feet[0].type);
-    setmyOuterwear(male.coats[0].type);
+    const generatedOutfit = [];
+    Object.keys(male).forEach((key) => {
+      generatedOutfit.push(
+        male[key][Math.floor(Math.random() * male[key].length)]
+      );
+    });
+    setMyOutfit(generatedOutfit);
   };
   return (
     <div>
       <h3>Select wardrobe</h3>
       <button onClick={chooseMyOutfit}>Click me</button>
-      <p>outfit is: {myBottoms}</p>
-      <p>{myTops}</p>
-      <p>{myFeet}</p>
-      <p>{myOuterwear}</p>
+      <p>Your outfit today is: </p>
+      <ul>
+        {Object.keys(myOutfit).map((key) => {
+          return (
+            <li key={key}>{`${myOutfit[key].colour} ${
+              myOutfit[key].make ? myOutfit[key].make : ""
+            }  ${
+              myOutfit[key].designFeature ? myOutfit[key].designFeature : ""
+            } ${myOutfit[key].type}`}</li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
